0012f6d39bce767cec0bceafe05ee47997639a50
likithaaa <likithad1@gmail.com>
2020-10-11 12:40:39 -0400

    Data is being fetched from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const API_URL = 'https://opentdb.com/api.php?amount=10&type=multiple';
+
 function App() {
-  return <div>Hello!</div>;
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <div>Question</div>
+      <div>
+        <div>Answer 1</div>
+        <div>Answer 2</div>
+        <div>Answer 3</div>
+        <div>Answer 4</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
