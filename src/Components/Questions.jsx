28131f815090b73cb786456ffa97878e21016c6b
likithaaa <likithad1@gmail.com>
2020-10-12 01:22:21 -0400

    Finish styling

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -8,36 +8,50 @@ const Question = ({
   data: { question, correct_answer, answers },
 }) => {
   return (
-    <div className="flex flex-col">
-      <div className="bg-white text-indigo-700 p-10 rounded shadow-md">
-        <h2 dangerouslySetInnerHTML={{ __html: question }}></h2>
-      </div>
+    <div>
+      <p className="triviaHut">
+        <span className="color1">T</span>
+        <span className="color2">r</span>
+        <span className="color3">i</span>
+        <span className="color4">v</span>
+        <span className="color5">i</span>
+        <span className="color6">a</span>
+        <span className="color7">H</span>
+        <span className="color8">u</span>
+        <span className="color9">t</span>
+      </p>
 
-      <div className="grid grid-cols-2 gap-6 mt-6">
-        {answers.map((answer) => {
-          const color = showAnswers
-            ? answer === correct_answer
-              ? 'bg-green-500'
-              : 'bg-red-500'
-            : 'bg-white';
+      <div className="flex flex-col">
+        <div className="bg-white text-indigo-600 p-10 rounded font-bold text-base shadow-md">
+          <h2 dangerouslySetInnerHTML={{ __html: question }}></h2>
+        </div>
 
-          return (
-            <button
-              className={`${color} p-4 text-purple-800 font-semibold rounded shadow`}
-              onClick={() => handleAnswer(answer)}
-              dangerouslySetInnerHTML={{ __html: answer }}
-            />
-          );
-        })}
+        <div className="grid grid-cols-2 gap-6 mt-6">
+          {answers.map((answer) => {
+            const color = showAnswers
+              ? answer === correct_answer
+                ? 'bg-green-200'
+                : 'bg-red-300'
+              : 'bg-white';
+
+            return (
+              <button
+                className={`${color} p-4 text-indigo-500 font-semibold rounded shadow`}
+                onClick={() => handleAnswer(answer)}
+                dangerouslySetInnerHTML={{ __html: answer }}
+              />
+            );
+          })}
+        </div>
+        {showAnswers && (
+          <button
+            onClick={handleNextQuestion}
+            className={`ml-auto bg-indigo-200 text-indigo-700 p-3 font-semibold rounded shadow mt-5`}
+          >
+            Next Question
+          </button>
+        )}
       </div>
-      {showAnswers && (
-        <button
-          onClick={handleNextQuestion}
-          className={`ml-auto bg-purple-300 text-purple-800  p-4 font-semibold rounded shadow mt-5`}
-        >
-          Next Question
-        </button>
-      )}
     </div>
   );
 };
